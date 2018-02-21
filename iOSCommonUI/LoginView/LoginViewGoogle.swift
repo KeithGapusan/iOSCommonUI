5d4c17a78d69c1f809a9369d7b487074599aff8f
idah <bsnatividad@gmail.com>
2018-02-21 11:39:36 +0800

    Fix UI issues first QA in Login, Profile, News and Events

--- iOSCommonUI/LoginView/LoginViewGoogle.swift
+++ iOSCommonUI/LoginView/LoginViewGoogle.swift
@@ -9,20 +9,19 @@
 import UIKit
 
 public class LoginViewGoogle: UIView, UITextFieldDelegate {
+    
+    @IBOutlet public weak var btnGoogle: UIButton!
+    @IBOutlet public weak var btnLogin: UIButton!
+    @IBOutlet public weak var btnSignUp: UIButton!
+    @IBOutlet public weak var btnForgotPass: UIButton!
+    @IBOutlet public weak var ivLogo: UIImageView!
         
-        
-       
-        @IBOutlet public weak var btnGoogle: UIButton!
-        @IBOutlet public weak var btnLogin: UIButton!
-        @IBOutlet public weak var btnSignUp: UIButton!
-        @IBOutlet public weak var btnForgotPass: UIButton!
-        @IBOutlet public weak var ivLogo: UIImageView!
-        
-        @IBOutlet public weak var tfUserName: UITextField!
-        @IBOutlet public weak var tfPassword: UITextField!
-        @IBOutlet public var view: UIView!
-        
-        public required init?(coder aDecoder: NSCoder) {
+    @IBOutlet public weak var tfUserName: UITextField!
+    @IBOutlet public weak var tfPassword: UITextField!
+    @IBOutlet public var view: UIView!
+    @IBOutlet public weak var lblVersion: UILabel!
+    
+    public required init?(coder aDecoder: NSCoder) {
             super .init(coder: aDecoder)
             let bundleIdentifier = Bundle(for: LoginViewGoogle.self)
             UINib(nibName: "LoginViewGoogle", bundle: bundleIdentifier).instantiate(withOwner: self, options: nil)
