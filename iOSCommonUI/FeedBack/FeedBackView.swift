14732e3165e44650add1d3e788c4be465c995908
Keith Gapusan <keithgapusan@samcd-15015s-imac.local>
2018-04-11 14:52:20 +0800

    Update the iOSCommonUI for login and feedback

---
+++ iOSCommonUI/FeedBack/FeedBackView.swift
@@ -0,0 +1,18 @@
+//
+//  FeedBackView.swift
+//  iOSCommonUI
+//
+//  Created by Keith Gapusan on 11/04/2018.
+//  Copyright © 2018 Keith Randell Gapusan. All rights reserved.
+//
+
+import UIKit
+
+class FeedBackView:UIView {
+
+
+    @IBAction func didPressedButton(_ sender: Any) {
+        
+    }
+    
+}
