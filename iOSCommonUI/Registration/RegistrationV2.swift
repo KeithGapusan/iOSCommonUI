11b2e273ab496b511a3682fd95fbeb936de1fd0f
Keith Gapusan <keithgapusan@samcd-15015s-imac.local>
2018-05-10 10:38:50 +0800

    Created a RegistrationView v2 Implement new design

---
+++ iOSCommonUI/Registration/RegistrationV2.swift
@@ -0,0 +1,13 @@
+//
+//  RegistrationV2.swift
+//  iOSCommonUI
+//
+//  Created by Keith Gapusan on 10/05/2018.
+//  Copyright © 2018 Keith Randell Gapusan. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationV2: UIView{
+
+}
