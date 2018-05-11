acd53a42da88f47223786020dc57cfe45a8d89e3
Keith Gapusan <keithgapusan@samcd-15015s-imac.local>
2018-05-11 13:11:53 +0800

    Additional view - Forgot password Registration v2 class  -  Implement delegates and initialization of view

--- iOSCommonUI/Registration/RegistrationV2.swift
+++ iOSCommonUI/Registration/RegistrationV2.swift
@@ -7,7 +7,60 @@
 //
 
 import UIKit
+public protocol RegistrationV2Delegate{
+    func didPressedButton(sender: [String:Any])
+}
 
-class RegistrationV2: UIView{
+public class RegistrationV2: UIView{
 
+    @IBOutlet weak var tfName: UITextField!
+    @IBOutlet weak var tfLastName: UITextField!
+    @IBOutlet weak var tfEmail: UITextField!
+    @IBOutlet weak var tfPassword: UITextField!
+    @IBOutlet weak var tfConfirmPassword: UITextField!
+    @IBOutlet weak var btnLogin: UIButton!
+    @IBOutlet weak var btnAlreadyMember: UIButton!
+    @IBOutlet var view: RegistrationV2!
+    public var delegate: RegistrationV2Delegate!
+    @IBAction func didPressedButton(_ sender: Any) {
+        let btn = sender as! UIButton
+        if btn == btnLogin{
+            let data = ["first_name":tfName.text! , "last_name":tfLastName.text!, "email":tfEmail.text!, "password": tfPassword.text!]
+            delegate.didPressedButton(sender: data)
+        }else if btn == btnAlreadyMember{
+            
+        }else{
+            print("not applicable")
+        }
+    }
+    
+    
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+        xibSetup()
+    }
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        //  xibSetup()
+        //uncomment this if you want to implement this view directly in your Interface Builder View
+    }
+    public func instanceFromNib() -> UIView {
+        // tableView.register(UINib(nibName:xibName.headerSeparatorCell, bundle: nil), forCellReuseIdentifier: cellIdentifier.header_separator)
+        let bundleIdentifier = Bundle(for: RegistrationV2.self)
+        print("bundle ID == \(bundleIdentifier)")
+        
+        return UINib(nibName: xibName.registrationV2, bundle: bundleIdentifier).instantiate(withOwner: self, options: nil)[0] as! UIView
+    }
+    func xibSetup() {
+        view = self.instanceFromNib() as! RegistrationV2
+        
+        // use bounds not frame or it'll be offset
+        view.frame = bounds
+        
+        // Make the view stretch with containing view
+        view.autoresizingMask = [UIViewAutoresizing.flexibleWidth, UIViewAutoresizing.flexibleHeight]
+        // Adding custom subview on top of our view (over any custom drawing > see note below)
+        addSubview(view)
+        
+    }
 }
