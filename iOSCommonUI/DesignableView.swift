31512849ddec6e77b60465f72fb420bfd2d1e420
keith31 <ksgapusan@straightarrow.com.ph>
2018-05-29 22:49:36 +0800

    Added new Placeholder designable on textfield

--- iOSCommonUI/DesignableView.swift
+++ iOSCommonUI/DesignableView.swift
@@ -27,6 +27,33 @@ class DesignableTextView: UITextView {
 }
 
 
+extension UITextField {
+//    @IBInspectable var placeholderColor: UIColor {
+//        get {
+//            return attributedPlaceholder?.attribute(.foregroundColor, at: 0, effectiveRange: nil) as? UIColor ?? .clear
+//        }
+//        set {
+//            guard let attributedPlaceholder = attributedPlaceholder else { return }
+//            let attributes: [NSAttributedStringKey: UIColor] = [.foregroundColor: newValue]
+//            self.attributedPlaceholder = NSAttributedString(string: attributedPlaceholder.string, attributes: attributes)
+//        }
+//    }
+    
+    
+    @IBInspectable var placeholderColor: UIColor {
+        get {
+            guard let currentAttributedPlaceholderColor = attributedPlaceholder?.attribute(NSForegroundColorAttributeName, at: 0, effectiveRange: nil) as? UIColor else { return UIColor.clear }
+            return currentAttributedPlaceholderColor
+        }
+        set {
+            guard let currentAttributedString = attributedPlaceholder else { return }
+            let attributes = [NSForegroundColorAttributeName : newValue]
+            
+            attributedPlaceholder = NSAttributedString(string: currentAttributedString.string, attributes: attributes)
+        }
+    }
+}
+
 extension UIView {
 
 
