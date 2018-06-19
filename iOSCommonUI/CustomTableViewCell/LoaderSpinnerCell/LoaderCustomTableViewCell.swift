92a95e842069507865d0d1c113f6b61d88697f3e
Keith Gapusan <keithgapusan@SAMCD-15015s-iMac.local>
2018-06-19 14:49:04 +0800

    added  - custom loader cell

---
+++ iOSCommonUI/CustomTableViewCell/LoaderSpinnerCell/LoaderCustomTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  LoaderCustomTableViewCell.swift
+//  StraightArrow
+//
+//  Created by Keith Gapusan on 24/10/2017.
+//  Copyright © 2017 Keith Randell Gapusan. All rights reserved.
+//
+
+import UIKit
+
+public class LoaderCustomTableViewCell: UITableViewCell {
+
+    override public func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override public func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    public func getNib() -> UINib{
+        return UINib(nibName:xibName.loaderCell, bundle: Bundle(for: HeaderTableViewCell.self))
+    }
+    public func getCellId() -> String{
+        return cellIdentifier.loaderCell
+    }
+    public func registerNib(tableView:UITableView){
+        tableView.register(getNib(), forCellReuseIdentifier: getCellId())
+    }
+    
+}
